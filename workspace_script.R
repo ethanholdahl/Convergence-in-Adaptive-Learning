4e3b84a142799613d1f03942f8c8599b118b2f7b
Ethan Holdahl <eholdahl@uoregon.edu>
2020-04-04 12:37:48 -0700

    scenario selection finished.

--- workspace_script.R
+++ workspace_script.R
@@ -7,7 +7,7 @@ scenario = "switch different"
 scenario = "switch same"
 
 hA = sample(c(0,1), size = m, replace = TRUE)
-hA = sample(c(0,1), size = m, replace = TRUE)
+hB = sample(c(0,1), size = m, replace = TRUE)
 
 sA = tail(hB,s)
 sB = tail(hA,s)
@@ -15,7 +15,7 @@ sB = tail(hA,s)
 aA = ifelse(sum(sA/s) > a, 1, 0)
 aB = ifelse(sum(sB/s) > b, 1, 0)
 
-while (scenario == "match" & a1 != a2){
+while (scenario == "match" & aA != aB){
   hA = sample(c(0,1), size = m, replace = TRUE)
   hB = sample(c(0,1), size = m, replace = TRUE)
   
@@ -36,13 +36,61 @@ while (scenario == "switch different"){
   aA = ifelse(sum(sA/s) > a, 1, 0)
   aB = ifelse(sum(sB/s) > b, 1, 0)
   if (aA == aB) next
-  if (aA == 1){ #player A will play 1 and player B will play 0
-    #define k, j number of actions until player A, B's BR changes
-    k = 
-    j = 
+  #define rA and rB as the number of records in sample matching aA and aB respectively that need to be removed for the BR to change.
+  rA = ifelse(aA == 1, ceiling((1-a)*s-sum(sA==0)), ceiling(a*s-sum(sA)))
+  rB = ifelse(aB == 1, ceiling((1-a)*s-sum(sB==0)), ceiling(a*s-sum(sB)))
+  #initialize k, j: the number of actions until player A, B's BR changes.
+  k = rA
+  j = rB
+  while (sum(sA[1:k]==aA)!=rA){
+    k = k+1
   }
+  while (sum(sB[1:j]==aB)!=rB){
+    j = j+1
+  }
+  if (j == k) break
 }
 
-h1 = c(h1,a1)
-h2 = c(h2,a2)
+while (scenario == "switch same"){
+  hA = sample(c(0,1), size = m, replace = TRUE)
+  hB = sample(c(0,1), size = m, replace = TRUE)
+  
+  sA = tail(hB,s)
+  sB = tail(hA,s)
+  
+  aA = ifelse(sum(sA/s) > a, 1, 0)
+  aB = ifelse(sum(sB/s) > b, 1, 0)
+  if (aA == aB) next
+  #define rA and rB as the number of records in sample matching aA and aB respectively that need to be removed for the BR to change.
+  rA = ifelse(aA == 1, ceiling((1-a)*s-sum(sA==0)), ceiling(a*s-sum(sA)))
+  rB = ifelse(aB == 1, ceiling((1-a)*s-sum(sB==0)), ceiling(a*s-sum(sB)))
+  #initialize k, j: the number of actions until player A, B's BR changes.
+  k = rA
+  j = rB
+  while (sum(sA[1:k]==aA)!=rA){
+    k = k+1
+  }
+  while (sum(sB[1:j]==aB)!=rB){
+    j = j+1
+  }
+  if (j == k) break
+}
+
+
+sA = tail(hB,s)
+sB = tail(hA,s)
+
+aA = ifelse(sum(sA/s) > a, 1, 0)
+aB = ifelse(sum(sB/s) > b, 1, 0)
+
+sA
+sB
+aA
+aB
+
+hA = c(hA,aA)
+hB = c(hB,aB)
+
+hA
+hB
 

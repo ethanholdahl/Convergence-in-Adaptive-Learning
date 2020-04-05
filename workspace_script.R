7d84acc27efd27e56ab4d9d0e490a40764967e77
Ethan Holdahl <eholdahl@uoregon.edu>
2020-04-05 13:36:50 -0700

    Finished "switch same" evolution

--- workspace_script.R
+++ workspace_script.R
@@ -7,8 +7,8 @@ s = 13
 a = 1/2
 b = 1/2
 #scenario = "match"
-scenario = "switch different"
-#scenario = "switch same"
+#scenario = "switch different"
+scenario = "switch same"
 
 #randomly generating histories and taking most recent samples and calculating next actions
 hA = sample(c(0,1), size = m, replace = TRUE)
@@ -30,6 +30,10 @@ while (scenario == "match" & aA != aB){
   
   aA = ifelse(sum(sA/s) > a, 1, 0)
   aB = ifelse(sum(sB/s) > b, 1, 0)
+  
+  #added j,k = 0 for appropriate evolution length
+  j = 0
+  k = 0
 } 
 
 while (scenario == "switch different"){
@@ -94,28 +98,62 @@ while (scenario == "switch same"){
   if (j == k) break
 }
 
+#record the evolution of memory in data frames.
+jk = min(j,k)
+memory_slots = character(m)
+
+for (i in 1:(m)){
+  memory_slots[m+1-i] = (paste0("m",i))
+}
+
+hA_evo = data.frame(matrix(nrow = jk+m, ncol = m))
+hB_evo = data.frame(matrix(nrow = jk+m, ncol = m))
+
+colnames(hA_evo) = memory_slots
+colnames(hB_evo) = memory_slots
+
 #selecting possible samples until a convention is achieved.
 if (scenario != "switch same"){
-  #If scenario does is not "switch same" we can ensure a convention is met by sampling
-  #the most recent s records s+m times (s to ensure coordination, m to fill memory).
-  #record the evolution of memory in a data frame.
-  memory_slots = character(m)
-  
-  for (i in 1:(m)){
-    memory_slots[m+1-i] = (paste0("m",i))
-  }
-  
-  hA_evo = data.frame(matrix(nrow = s+m, ncol = m))
-  hB_evo = data.frame(matrix(nrow = s+m, ncol = m))
-  
-  colnames(hA_evo) = memory_slots
-  colnames(hB_evo) = memory_slots
+  #If scenario is not "switch same" we can ensure a convention is met by sampling
+  #the most recent s records jk+m times (jk periods until coordination, m to fill memory).
   
   i = 0
-  while (i < (s+m)){
+  while (i < (jk+m)){
     i = i+1
     
     sA = tail(hB,s)
+    sB = tail(hA,s)
+    
+    aA = ifelse(sum(sA/s) > a, 1, 0)
+    aB = ifelse(sum(sB/s) > b, 1, 0)
+    
+    hA = c(hA,aA)
+    hB = c(hB,aB)
+    
+    hA_evo[i,] = (tail(hA,m))
+    hB_evo[i,] = (tail(hB,m))
+  }
+} else {
+  #Scenario is "same switch". Since both best responses change after the same period
+  #we simply need to change the way one player (A) samples to get them to coordinate.
+  #We can reach a convention if the one player (B) samples the most recent s records
+  #for jk+m periods and the other player (A) samples in following way:
+  #1. For the first jk-1 periods sample the most recent s records
+  #2. For periods jk through jk+s sample the most recent s+1 records except for the record
+  #   created in period jk-1
+  #3. For periods jk+s+1 through jk+m sample the most recent s records
+  #This works because 2. stops player A from flipping their BR until their whole sample is
+  #the same action.
+  
+  i = 0
+  while (i < (jk+m)){
+    i = i+1
+    
+    if (i > (jk-1) & i < (jk+s+1)){
+      sA = tail(hB,s+1)[-(jk+s+1-i)]
+    } else {
+      sA = tail(hB,s)
+    }
     sB = tail(hA,s)
     
     aA = ifelse(sum(sA/s) > a, 1, 0)

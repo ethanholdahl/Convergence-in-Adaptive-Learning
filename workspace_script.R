3055a3045187dd0aebdec25ec81358bf7cbfa395
Ethan Holdahl <eholdahl@uoregon.edu>
2020-04-05 10:14:44 -0700

    added comments, working on adding data frame of evolving memory

--- workspace_script.R
+++ workspace_script.R
@@ -1,11 +1,16 @@
+#This script allows users to select paramaters for adaptive play and observe a possible path
+#to a convention under no pertubations.
+
+#Paramater selection
 m = 17
 s = 13
 a = 1/2
 b = 1/2
-scenario = "match"
+#scenario = "match"
 scenario = "switch different"
-scenario = "switch same"
+#scenario = "switch same"
 
+#randomly generating histories and taking most recent samples and calculating next actions
 hA = sample(c(0,1), size = m, replace = TRUE)
 hB = sample(c(0,1), size = m, replace = TRUE)
 
@@ -15,6 +20,7 @@ sB = tail(hA,s)
 aA = ifelse(sum(sA/s) > a, 1, 0)
 aB = ifelse(sum(sB/s) > b, 1, 0)
 
+#randomly regenerating histories until one that is consistent with desired scenario is generated.
 while (scenario == "match" & aA != aB){
   hA = sample(c(0,1), size = m, replace = TRUE)
   hB = sample(c(0,1), size = m, replace = TRUE)
@@ -74,6 +80,27 @@ while (scenario == "switch same"){
     j = j+1
   }
   if (j == k) break
+}
+
+#selecting possible samples until a convention is achieved.
+if (scenario != "switch same"){
+  #If scenario does is not "switch same" we can ensure a convention is met by sampling
+  #the most recent s records s+m times (s to ensure coordination, m to fill memory)
+  i = 0
+  while (i < (s+m)){
+    sA = tail(hB,s)
+    sB = tail(hA,s)
+    
+    aA = ifelse(sum(sA/s) > a, 1, 0)
+    aB = ifelse(sum(sB/s) > b, 1, 0)
+    
+    hA = c(hA,aA)
+    hB = c(hB,aB)
+    
+    print(tail(hA,m))
+    print(tail(hB,m))
+    i = i+1
+  }
 }
 
 
